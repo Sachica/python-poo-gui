af285c2e16747d0f1b48608cb940596e8fc5b1dd
Sachica <rojassachicadf@ufps.edu.co>
2022-08-06 14:47:48 -0500

    Actualización del archivo scan para futura funcionalidad Los cambios y metodos realizados fueron documentados

--- src/main/resources/scan.py
+++ src/main/resources/scan.py
@@ -10,24 +10,17 @@ class ClasePython:
     def __init__(self, nombre, path_module):
         self.nombre = nombre
         self.path_module = path_module
-        self.herencia = []
-    
-    def __get_list_herencia_json(self):
-        list_herencia_json = []
-        for herencia in self.herencia:
-            list_herencia_json.append(str(herencia))
-        return list_herencia_json
+        self.bases = []
 
     def to_json(self):
         return {
             "name": self.nombre,
             "pathModule": self.path_module,
-            "bases": self.__get_list_herencia_json()
+            "bases": [str(x) for x in self.bases]
         }
     
     def __str__(self) -> str:
         return self.path_module +'.'+ self.nombre
-
 
 class ArchivoPython:
     def __init__(self, file, name):
@@ -35,17 +28,11 @@ class ArchivoPython:
         self.name = name
         self.clases = []
 
-    def __get_list_classes_json(self):
-        list_classes_json = []
-        for clase in self.clases:
-            list_classes_json.append(str(clase))
-        return list_classes_json
-
     def to_json(self):
         return {
             "ficheroStr": self.file,
             "module": self.name,
-            "classes": self.__get_list_classes_json()
+            "classes": [str(x) for x in self.clases]
         }
 
     def __str__(self) -> str:
@@ -90,28 +77,85 @@ class Directorio:
             if archivo.file == name:
                 return archivo
         return None
-    
-    def __get_list_files_json(self):
-        list_files = []
-        for archivo in self.archivos:
-            list_files.append(str(archivo))
-        return list_files
-
-    def __get_list_folders_json(self):
-        list_folders = []
-        for directorio in self.directorios:
-            list_folders.append(str(directorio))
-        return list_folders
 
     def to_json(self):
         return {
             "ficheroStr": self.directorio,
-            "files": self.__get_list_files_json(),
-            "directorys": self.__get_list_folders_json()
+            "files": [str(x) for x in self.archivos],
+            "directorys": [str(x) for x in self.directorios]
         }
 
     def __str__(self) -> str:
         return self.directorio
+
+class AttrInstancia:
+    def __init__(self, key, value, type, to_reference):
+        self.key = key
+        self.value = value
+        self.type = type
+        self.rawtype = type.split(" ")[1][:-1]
+        self.to_reference = to_reference
+
+    def to_json(self):
+        return {
+            "key": self.key,
+            "value": str(self.value).replace("'", ""),
+            "type": self.type,
+            "rawtype": self.rawtype,
+            "to_reference": json.dumps(self.to_reference)
+        }
+    
+    def __str__(self):
+        return str(self.key)
+
+class MethodInstancia:
+    def __init__(self, name, args):
+        self.name = name
+        self.args = args
+    
+    def to_json(self):
+        return {
+            "name": self.name,
+            "args": self.args
+        }
+    
+    def __str__(self):
+        return str(self.name)
+
+class MundoInstancia:
+    def __init__(self, id, name_var, name_class, iscollection):
+        self.id = id
+        self.name = name_var
+        self.isdeclared = name_var != None
+        self.name_class = name_class.split(".")[-1].replace("'>", "")
+        self.attrs = []
+        self.methods = []
+        self.rawval = None
+        self.iscollection = iscollection
+
+    def append_attr(self, attr):
+        self.attrs.append(attr)
+
+    def append_method(self, method):
+        self.methods.append(method)
+    
+    def set_raw_val(self, rawval):
+        self.rawval = rawval
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name if self.name else "none",
+            'name_class': self.name_class,
+            'attrs': [x.to_json() for x in self.attrs], 
+            'methods': [x.to_json() for x in self.methods],
+            'rawval': str(self.rawval).replace("'", "").replace('"', ""),
+            'isdeclared': json.dumps(self.isdeclared),
+            'iscollection': json.dumps(self.iscollection)
+        }
+    
+    def __str__(self):
+        return str(self.name)
 
 #Obtiene el archivo las lineas de inicio y fin de una clase
 def get_lines_class(class_name):
@@ -157,37 +201,123 @@ def list_all_instancias():
     #Lista todas las clases pertenecientes al proyecto
     classes = [cls for cls in inspect.getmembers(sys.modules[__name__], inspect.isclass) if cls[1].__module__ != '__main__']
 
-    classes_values = []
+    list_ids = []
+    dict_instances = {}
     for key, value in local_val.items():
         for cls in classes:
             #Valida si el objeto es una instancia de alguna de las clases listadas
-            #ademas valida que pertenezcan a la misma clase ya que por ejemplo:
+            #ademas valida que pertenezcan a la misma clase ya que por ejemplo
             #si la instancia[value] es subclase de [cls[1]] retornara True
             if isinstance(value, cls[1]) and value.__class__ == cls[1]:
-                attr_value = {'attrs': [], 'methods': [], 'name_class': cls[0], 'name': key}
-                # attribute is a string representing the attribute name
-                for attribute in dir(local_val[key]):
-                    # Get the attribute value
-                    attribute_value = getattr(local_val[key], attribute)
-                    # Check that it is callable
-                    if callable(attribute_value):
-                        # Filter all dunder (__ prefix) methods
-                        if attribute.startswith('__') == False:
-                            attr_value['methods'].append({
-                                'name': attribute,
-                                'args': [arg for arg in inspect.getfullargspec(attribute_value).args if arg != 'self']
-                            })
-                for atkey in local_val[key].__dict__.keys():
-                    attr_value['attrs'].append({
-                        'key': atkey,
-                        'value': str(local_val[key].__dict__[atkey]),
-                        'type': str(type(local_val[key].__dict__[atkey])).replace("'", "")
-                    })
-                classes_values.append(attr_value)
+                if id(value) not in list_ids:
+                    list_ids.append(id(value))
+                    attr_value = inspect_and_get_instance_from(key, value, dict_instances, list_ids, classes)
+                    dict_instances[str(id(value))] = attr_value
+                else:
+                    id_str = str(id(value))
+                    dict_instances[id_str].isdeclared = True
+                    dict_instances[id_str].name = key
                 break
+    list_instances = [{k: v.to_json()} for k, v in dict_instances.items()]
+    #print(json.dumps(json.loads(str(list_ids).replace("'", '"')), indent=4))
+    #print(json.dumps(json.loads(str(list_instances).replace("'", '"')), indent=4))
+    print("list_all_instancias:"+json.dumps(json.loads(str(list_instances).replace("'", '"'))))
 
-    print("list_all_instancias:"+json.dumps(json.loads(str(classes_values).replace("'", '"'))))
+def inspect_and_get_instance_from(name, obj, dict_instances, list_ids, classes):
+    attr_value = MundoInstancia(id(obj), name, str(type(obj)), iscollection(obj))
+    # attribute is a string representing the attribute name
+    for attribute in dir(obj):
+        # Get the attribute value
+        attribute_value = getattr(obj, attribute)
+        # Check that it is callable
+        if callable(attribute_value):
+            # Filter all dunder (__ prefix) methods
+            if not attribute.startswith('__'):
+                attr_value.append_method(MethodInstancia(
+                    attribute,
+                    [arg for arg in inspect.getfullargspec(attribute_value).args if arg != 'self']
+                ))
+    for atkey in obj.__dict__.keys():
+        value = obj.__dict__[atkey]
+        typeval = type(value)
 
+        iscollect = iscollection(value)
+        isfromproject = isinstance_from_class_project(value, classes)
+        isregistered = id(value) in list_ids
+        isself = id(value) == id(obj)
+        isreference = iscollect or isfromproject or isregistered
+
+        if isfromproject:
+            if not isregistered:
+                strategy_for_classproject_or_collection(value, dict_instances, list_ids, classes)
+                value = id(value)
+
+            elif isself:
+                value = id(value)
+
+        elif iscollect:
+            if not isregistered:
+                strategy_for_classproject_or_collection(value, dict_instances, list_ids, classes)
+            value = id(value)
+
+        attr_value.append_attr(AttrInstancia(
+            atkey,
+            value,
+            str(typeval).replace("'", ""),
+            isreference
+        ))
+
+    return attr_value
+
+#Itera una python collection 'obj' en busca de instancias de clases del proyecto
+def inspect_for_collection(obj, dict_instances, list_ids, classes):
+    if isinstance(obj, list) or isinstance(obj, tuple) or isinstance(obj, set):
+        res = []
+        for x in obj:
+            res.append(inspect_and_get_apropiate_value(x, dict_instances, list_ids, classes))
+        return res if isinstance(obj, list) else (tuple(res) if isinstance(obj, tuple) else set(res))
+
+    if isinstance(obj, dict):
+        res = {}
+        for k, v in obj.items():
+            res[k] = inspect_and_get_apropiate_value(v, dict_instances, list_ids, classes)
+        return res
+
+#Inspecciona si el valor es una instancia del proyecto o es una python collection, en ese
+#caso se tratara el objeto y retornara el id de referencia, en caso contrario el valor
+#es primitivo
+def inspect_and_get_apropiate_value(x, dict_instances, list_ids, classes):
+    isfromproject = isinstance_from_class_project(x, classes)
+    iscollect = iscollection(x)
+    if isfromproject or iscollect:
+        strategy_for_classproject_or_collection(x, dict_instances, list_ids, classes)
+        return id(x)
+    return x
+
+#Realiza un tratamiento al objeto 'x' en funcion de si es una python collection o
+#instancia de alguna clase del proyecto
+def strategy_for_classproject_or_collection(x, dict_instances, list_ids, classes):
+    attr_instance = None
+    list_ids.append(id(x))
+
+    if iscollection(x):
+        attr_instance = MundoInstancia(id(x), None, str(type(x)).replace("'", ""), True)
+        attr_instance.set_raw_val(str(inspect_for_collection(x, dict_instances, list_ids, classes)))
+    else:
+        attr_instance = inspect_and_get_instance_from(None, x, dict_instances, list_ids, classes)
+
+    dict_instances[str(id(x))] =  attr_instance
+
+#Valida si el objeto 'obj' es una instancia de alguna clase del proyecto
+def isinstance_from_class_project(obj, classes):
+    for _, clzztype in classes:
+        if type(obj) == clzztype:
+            return True
+    return False 
+
+#Valida si el objeto 'obj' es una instancia de una python collection
+def iscollection(obj):
+    return isinstance(obj, list) or isinstance(obj, dict) or isinstance(obj, tuple) or isinstance(obj, set)
 
 def list_all_python_class_and_errors(folder_root: Directorio):
     #Almacena errores que se encuentren al importar un modulo[str]
@@ -235,7 +365,7 @@ def list_all_python_class_and_errors(folder_root: Directorio):
                                 if class_path_base not in dict_class_classpython:
                                     dict_class_classpython[class_path_base] = ClasePython(class_name_herencia, path_mod_herencia)
 
-                                clase_python.herencia.append(dict_class_classpython[class_path_base])
+                                clase_python.bases.append(dict_class_classpython[class_path_base])
             except Exception as err:
                 list_errores.append(str(err))
         for sub_dir in directorio.directorios:
